fc07b4fbe2f48f891286a4af114ca8d07f733cec
Your Name <your.email@example.com>
2025-01-16 22:22:54 +0530

    changed

--- api/server.js
+++ api/server.js
@@ -44,13 +44,17 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
-
+app.options('*', cors(corsOptions));
 app.use('/uploads', express.static('uploads'));
 
 
 app.use('/api/submission', submissionRoute);
 app.use('/api', dashboardRoute);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log('Server is running on port 8000');
 });
